865738afacef2fea4c754a5dee9ce76262ae1914
ifce <ifce@172-16-2-104.0000.ifce.br.iosda.org>
2017-09-11 19:55:30 -0300

    implement get list from server

--- tweeter/tweeter/RestAcessor.swift
+++ tweeter/tweeter/RestAcessor.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+
+struct RestAcessor {
+    
+    let baseURL = "https://ios-twitter.herokuapp.com"
+    let apiURL = "/api/v2/message"
+    var restResponderDelegate : RestResponderDelegate?
+    
+    init() {
+        
+    }
+    
+    func requestList(){
+        let completeURL = baseURL + apiURL
+        let requestURL = URL(string: completeURL)
+        var request = URLRequest(url: requestURL!)
+        
+        request.httpMethod = "GET"
+        
+        
+        let task = URLSession.shared.dataTask(with: request){
+            data, response, error in
+            
+            if error != nil{
+                print("Error")
+                return
+            }
+            
+            let json = try? JSONSerialization.jsonObject(with: data!, options: []) as? [Any]
+            
+            var tweets = [Tweet]()
+            for case let result in json!!{
+                if let tweet = Tweet(json: result as! [String : Any]){
+                    tweets.append(tweet)
+                }
+            }
+            
+//            print(tweets)
+            
+//            let responseString = String(data: data!, encoding: .utf8)
+            
+            self.restResponderDelegate?.getListResponse(tweets: tweets)
+        }
+        
+        task.resume()
+        
+    }
+    
+    func requestDeleteTweet(){
+        
+    }
+    
+    func requestCreateTweet(){
+        
+    }
+    
+    func requestEditTweet(){
+        
+    }
+    
+}
+
+
+protocol RestResponderDelegate{
+    func getListResponse(tweets: [Tweet])
+    func deleteTweetResponse(tweet : Tweet)
+    func postResponse()
+    func editResponse(tweet : Tweet)
+}
---
+++ tweeter/tweeter/Tweet.swift
@@ -0,0 +1,52 @@
+//
+//  Tweet.swift
+//  tweeter
+//
+//  Created by ifce on 11/09/17.
+//  Copyright © 2017 ifce. All rights reserved.
+//
+
+import Foundation
+
+
+class Tweet{
+    var _id : String
+    var text: String
+    var lat : Double
+    var long : Double
+    var created_at : String
+    var updated_at: String
+    
+    init(_id: String, text: String, lat : Double, long : Double, created_at:String, updated_at : String) {
+        self._id = _id
+        self.text = text
+        self.lat = lat
+        self.long = long
+        self.created_at = created_at
+        self.updated_at = updated_at
+    }
+    
+    init?(json: [String: Any]) {
+        
+        guard let id = json["_id"] as? String,
+            let text = json["text"] as? String,
+            let latitude = json["lat"] as? Double,
+            let longitude = json["long"] as? Double,
+            let creation = json["created_at"] as? String,
+            let update = json["updated_at"] as? String
+            else{
+                return nil
+        }
+        
+        self._id = id
+        self.text = text
+        self.lat = latitude
+        self.long = longitude
+        self.created_at = creation
+        self.updated_at = update
+        
+        
+    }
+    
+    
+}
